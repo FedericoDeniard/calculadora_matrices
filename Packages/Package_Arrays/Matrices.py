fb1004f4c87b53ac3a6bcd84a2c05dd78a01ea74
Federico Deniard <fededeniard@gmail.com>
2024-05-15 19:23:52 -0300

    added subtract and sum

--- Packages/Package_Arrays/Matrices.py
+++ Packages/Package_Arrays/Matrices.py
@@ -9,10 +9,14 @@ def new_array() -> list:
     """
     rows = get_int("Ingrese la cantidad de filas del array: ")
     columns = get_int("Ingrese la cantidad de columnas del array: ")
-    array = [[0] * columns for _ in range(rows)]
+    array = [[f"x{i}{j}" for j in range(1, columns + 1)] for i in range(1, rows + 1)]
     for i in range(len(array)):
         for j in range(len(array[0])):
-            array[i][j] = get_int(f"Ingrese el valor de la posicion [{i+1}][{j+1}]: ")
+            system("cls")
+            print(show_array(array))
+            array[i][j] = get_int(f"Ingrese el valor de la posicion x{i+1}{j+1}: ")
+
+
 
     return array
 
@@ -34,7 +38,7 @@ def can_multiply(A: list, B: list) -> bool:
         validate = True
     return validate
 
-def multiply_matrix(A: list, B: list) -> str:
+def multiply_matrix(A: list, B: list) -> list:
     """Multiplies 2 arrays without caring about validation
 
     Args:
@@ -61,6 +65,39 @@ def multiply_matrix(A: list, B: list) -> str:
 
     return C
 
+def sum_array() -> list:
+    print("Primera matriz")
+    A = new_array()
+    system("cls")
+    print("Segunda matriz")
+    B = new_array()
+    system("cls")
+    if len(A) == len(B) and len(A[0]) == len(B[0]):
+        C = [[f"x{i}{j}" for j in range(1, len(A[0]) + 1)] for i in range(1, len(A) + 1)]
+        for i in range(len(C)):
+            for j in range(len(C[0])):
+                C[i][j] = A[i][j] + B[i][j]
+    else:
+        C = []
+    return C
+
+def subtract_array() -> list:
+    print("Primera matriz")
+    A = new_array()
+    system("cls")
+    print("Segunda matriz")
+    B = new_array()
+    system("cls")
+    if len(A) == len(B) and len(A[0]) == len(B[0]):
+        C = [[f"x{i}{j}" for j in range(1, len(A[0]) + 1)] for i in range(1, len(A) + 1)]
+        for i in range(len(C)):
+            for j in range(len(C[0])):
+                C[i][j] = A[i][j] - B[i][j]
+    else:
+        C = []
+    return C
+
+
 def multiply_array() -> list:
     """ Calls other array methods and if succesfull, returns the product between, otherwise, returns an empty array.
 
@@ -78,3 +115,20 @@ def multiply_array() -> list:
     else:
         result = []
     return result
+
+def show_array(array: list):
+    """ Recieves an array and prints a formatted version of it.
+
+    Args:
+        array (list): The array you want to print.
+
+    """
+    message = "[ "
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            message += f"{array[i][j]} "
+        if i == len(array) - 1:
+            message += "]"
+        else:
+            message += "\n  "
+    return message
--- matrices.py
+++ matrices.py
@@ -8,24 +8,48 @@ Apellido: Deniard
 from Packages.Package_Arrays.Matrices import *
 from Packages.Package_Input.Input import *
 
-def print_array(array: list):
+def print_array(operation: int):
     """ Recieves an array and prints a formatted version of it.
 
     Args:
         array (list): The array you want to print.
 
     """
-    message = "[ "
-    for i in range(len(array)):
-        for j in range(len(array[i])):
-            message += f"{array[i][j]} "
-        if j == len(array[i]) -1:
-            if i == len(array[i]) -1:
-                message += "]"
+    match operation:
+        case 1:
+            result = multiply_array()
+            if result == []:
+                message = f"Las matrices no se pueden multiplicar"
+                print(message)
+            else:    
+                message = f"El producto de las dos matrices es:\n"
+                print(message + show_array(result))
+        case 2:
+            result = sum_array()
+            if result == []:
+                message = f"Las matrices no se pueden sumar"
+                print(message)
             else:
-                message += f"\n"
-    message = f"El producto de las dos matrices es:\n {message}"
-    print(message)
+                message = f"La suma de las dos matrices es:\n"
+                print(message + show_array(result))
+        case 3:
+            result = subtract_array()
+            if result == []:
+                message = f"Las matrices no se pueden restar"
+                print(message)
+            else:
+                message = f"La resta de las dos matrices es:\n"
+                print(message + show_array(result))
     system("pause")
 
-print_array(multiply_array())
+def menu():
+    run = True
+    while run:
+        option = get_int(message="1. Multiply 2 matrix\n2. Sum 2 matrix\n3. Subtract 2 matrix\n4. Exit\nChoose an operation: ",min=1,max=4)
+        match option:
+            case 4:
+                run = False
+            case _:
+                print_array(option)
+        system("cls")
+menu()

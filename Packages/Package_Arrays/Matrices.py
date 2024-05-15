0ee61c44fd658d10c753c648ec4c115d8f4e4cfc
Federico Deniard <fededeniard@gmail.com>
2024-05-15 19:29:06 -0300

    .

--- Packages/Package_Arrays/Matrices.py
+++ Packages/Package_Arrays/Matrices.py
@@ -132,3 +132,37 @@ def show_array(array: list):
         else:
             message += "\n  "
     return message
+
+def print_array(operation: int):
+    """ Recieves an array and prints a formatted version of it.
+
+    Args:
+        array (list): The array you want to print.
+
+    """
+    match operation:
+        case 1:
+            result = multiply_array()
+            if result == []:
+                message = f"Las matrices no se pueden multiplicar"
+                print(message)
+            else:    
+                message = f"El producto de las dos matrices es:\n"
+                print(message + show_array(result))
+        case 2:
+            result = sum_array()
+            if result == []:
+                message = f"Las matrices no se pueden sumar"
+                print(message)
+            else:
+                message = f"La suma de las dos matrices es:\n"
+                print(message + show_array(result))
+        case 3:
+            result = subtract_array()
+            if result == []:
+                message = f"Las matrices no se pueden restar"
+                print(message)
+            else:
+                message = f"La resta de las dos matrices es:\n"
+                print(message + show_array(result))
+    system("pause")
--- matrices.py
+++ matrices.py
@@ -8,40 +8,6 @@ Apellido: Deniard
 from Packages.Package_Arrays.Matrices import *
 from Packages.Package_Input.Input import *
 
-def print_array(operation: int):
-    """ Recieves an array and prints a formatted version of it.
-
-    Args:
-        array (list): The array you want to print.
-
-    """
-    match operation:
-        case 1:
-            result = multiply_array()
-            if result == []:
-                message = f"Las matrices no se pueden multiplicar"
-                print(message)
-            else:    
-                message = f"El producto de las dos matrices es:\n"
-                print(message + show_array(result))
-        case 2:
-            result = sum_array()
-            if result == []:
-                message = f"Las matrices no se pueden sumar"
-                print(message)
-            else:
-                message = f"La suma de las dos matrices es:\n"
-                print(message + show_array(result))
-        case 3:
-            result = subtract_array()
-            if result == []:
-                message = f"Las matrices no se pueden restar"
-                print(message)
-            else:
-                message = f"La resta de las dos matrices es:\n"
-                print(message + show_array(result))
-    system("pause")
-
 def menu():
     run = True
     while run:
